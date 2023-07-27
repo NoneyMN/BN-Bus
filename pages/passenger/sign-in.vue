b728c32995f59f1ebfe776f7b0e2d8e9edf36dad
NoneyMN <norasnoney.matnoor@gmail.com>
2023-07-27 08:11:11 +0800

    created base for passenger sign in page

--- pages/passenger/sign-in.vue
+++ pages/passenger/sign-in.vue
@@ -1,3 +1,29 @@
 <template>
-    <div>Passenger Sign In Page</div>
+  <div>
+    <v-container>
+      <v-row>
+        <v-btn @click="navigateTo('/index')">back</v-btn>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-img width="100" src="/images/green-bus.png"></v-img>
+          <h2>Welcome to<br />BN: Bus</h2>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-form>
+            <v-text-field label="Phone number"></v-text-field>
+            <v-text-field label="Password"></v-text-field>
+            <v-btn @click="navigateTo('/passenger/home')">Sign in</v-btn>
+          </v-form>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <p>New user? <a href="/passenger/sign-up">Sign up</a></p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
