f8bb3804f662313671b1de25b3741ecd6af066f7
NoneyMN <norasnoney.matnoor@gmail.com>
2023-07-29 02:41:45 +0800

    added scan ticket page content

--- pages/passenger/scan-ticket.vue
+++ pages/passenger/scan-ticket.vue
@@ -2,12 +2,50 @@
   <v-container>
     <v-row>
       <v-col cols="3">
-        <NavMenu />
+        <v-btn @click="navigateTo('/passenger/my-tickets')">back</v-btn>
       </v-col>
       <v-col cols="6">
         <h4 class="text-center">Ticket</h4>
       </v-col>
       <v-spacer></v-spacer>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-img height="200" src="/images/ticket-1.png"></v-img>
+        <p class="text-center">Ticket ID : 123ABC4DE5</p>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-divider></v-divider>
+      <v-col>
+        <h4>Bus 01</h4>
+        <p>Jalan Gadong to Jalan RIPAS</p>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-divider></v-divider>
+      <v-col>
+        <p>Pickup at</p>
+        <h4>Jalan Gadong Bus Stop</h4>
+        <br />
+        <p>Drop at</p>
+        <h4>Jalan RIPAS Bus Stop</h4>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-divider></v-divider>
+      <v-col cols="9">
+        <p>Amount Paid via Wallet</p>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col>
+        <h4>$1.00</h4>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="d-flex justify-center">
+        <v-btn color="black" @click="navigateTo('/passenger/bus-live-status')">Show bus live status</v-btn>
+      </v-col>
     </v-row>
   </v-container>
 </template>
