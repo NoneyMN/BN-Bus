9294cb833b7058b2cd74d071c9c33118f15ee7fb
NoneyMN <norasnoney.matnoor@gmail.com>
2023-07-29 02:50:42 +0800

    format with prettier

--- pages/passenger/sign-in.vue
+++ pages/passenger/sign-in.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-container>
-      <v-row>
+  <v-container>
+    <v-row>
       <v-col cols="3">
         <v-btn @click="navigateTo('/')">back</v-btn>
       </v-col>
@@ -9,25 +9,25 @@
       </v-col>
       <v-spacer></v-spacer>
     </v-row>
-      <v-row>
-        <v-col>
-          <v-img width="100" src="/images/green-bus.png"></v-img>
-          <h2>Welcome to<br />BN Bus</h2>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-form>
-            <v-text-field label="Phone number"></v-text-field>
-            <v-text-field label="Password"></v-text-field>
-            <v-btn @click="navigateTo('/passenger/home')">Sign in</v-btn>
-          </v-form>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <p>New user? <a href="/passenger/sign-up">Sign up</a></p>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-row>
+      <v-col>
+        <v-img width="100" src="/images/green-bus.png"></v-img>
+        <h2>Welcome to<br />BN Bus</h2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-form>
+          <v-text-field label="Phone number"></v-text-field>
+          <v-text-field label="Password"></v-text-field>
+          <v-btn @click="navigateTo('/passenger/home')">Sign in</v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <p>New user? <a href="/passenger/sign-up">Sign up</a></p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
