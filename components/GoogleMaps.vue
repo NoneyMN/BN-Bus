ccf9b50a7b2169667ac952f37ab23a1bd572a65c
NoneyMN <norasnoney.matnoor@gmail.com>
2023-07-29 09:56:06 +0800

    create map component

---
+++ components/GoogleMaps.vue
@@ -0,0 +1,10 @@
+<template>
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d7950.482281095013!2d114.92329849999993!3d4.8992561000000086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x3222f54530f0e11f%3A0xed80dbb2350da35d!2sThe%20Mall%20Gadong%2C%20Bandar%20Seri%20Begawan!3m2!1d4.9055786!2d114.91670119999999!4m5!1s0x32228acb18b46725%3A0x9118155b10d353ca!2sRaja%20Isteri%20Pengiran%20Anak%20Saleha%20Hospital%2C%20Jalan%20Putera%20Al-Muhtadee%20Billah%2C%20Bandar%20Seri%20Begawan!3m2!1d4.89263!2d114.9313054!5e0!3m2!1sen!2sbn!4v1690592505631!5m2!1sen!2sbn"
+      width="360"
+      height="595"
+      style="border: 0"
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+</template>
